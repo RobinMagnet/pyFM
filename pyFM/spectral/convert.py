ad4a390a3aac1d4660e3c8ede69cb20761fbe106
Robin <robmagnet1@gmail.com>
2021-03-26 10:54:20 +0100

    Fix import bug

--- pyFM/spectral/convert.py
+++ pyFM/spectral/convert.py
@@ -1,14 +1,13 @@
 from sklearn.neighbors import KDTree
 import scipy.linalg
 
-# try:
-#     import pynndescent
-#     index = pynndescent.NNDescent(np.random.random((100, 3)), n_jobs=2)
-#     del index
-#     ANN = True
-# except ImportError:
-#     ANN = False
-ANN = False
+try:
+    import pynndescent
+    index = pynndescent.NNDescent(np.random.random((100, 3)), n_jobs=2)
+    del index
+    ANN = True
+except ImportError:
+    ANN = False
 
 
 def p2p_to_FM(p2p, eigvects1, eigvects2, A2=None):
