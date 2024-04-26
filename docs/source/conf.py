e0fde08558c61f55d29692994e5b086bd6162b87
Robin Magnet <robmagnet1@gmail.com>
2024-04-26 18:17:45 +0200

    try fix on actions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,7 +7,7 @@
 import pathlib
 import sys
 sys.path.insert(0, pathlib.Path(__file__).parents[2].resolve().as_posix())
-
+print(pathlib.Path(__file__).parents[2].resolve().as_posix())
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
